ccc27ac815f4a6057993d024f7a3948bf535a0fc
99-zziy <wldud8229@gmail.com>
2023-07-31 23:03:27 +0900

    증가 감소 store 관리하기

--- src/app.js
+++ src/app.js
@@ -1,24 +1,46 @@
 import { create } from "./vanilla.js";
 
 // 스토어 생성
-const store = create((set) => ({
+const btn1 = create(() => ({
   count: 0,
 }));
 
-const counterDisplay = document.querySelector("#counter");
-const btnIncrease = document.querySelector("#btn-increase");
+const btn2 = create(() => ({
+  count: 10000,
+}));
+
+const counter1Display = document.querySelector("#counter1");
+const btn1Increase = document.querySelector("#btn-increase1");
+
+const counter2Display = document.querySelector("#counter2");
+const btn2Increase = document.querySelector("#btn-increase2");
 
 function increment() {
-  store.setState((state) => ({ count: state.count + 1 }));
+  btn1.setState((state) => ({ count: state.count + 1 }));
 }
 
-function logState(state) {
-  counterDisplay.textContent = state.count;
+function logState(state, previousState) {
+  counter1Display.textContent = state.count;
 }
 
 // 상태 변경을 구독
-store.subscribe(logState);
+btn1.subscribe(logState);
 
-btnIncrease.addEventListener("click", () => {
+btn1Increase.addEventListener("click", () => {
   increment();
+});
+
+function decrease() {
+  btn2.setState((state) => ({ count: state.count - 1 }));
+}
+
+function log2State(state, previousState) {
+  counter2Display.textContent = state.count;
+}
+
+// 상태 변경을 구독
+btn2.subscribe(log2State);
+
+btn2Increase.addEventListener("click", () => {
+  decrease();
 });
--- src/vanilla.js
+++ src/vanilla.js
@@ -1,21 +1,22 @@
-// createState는 예제에서 보았던 생성자 함수이다.
-// 함수 최하단부에 언급되지만 set, get 함수와
-// `create` 함수를 통해 만들어지는 내부 API를 인자로 전달받을 수 있다.
 export function create(createState) {
   // 스토어의 상태는 클로저로 관리된다.
   let state;
 
   // 상태 변경을 구독할 리스너를 Set으로 관리한다.
-  // 배열로 관리할 경우 중복된 리스너를 솎아내기 어렵기 때문이다.
   const listeners = new Set();
 
   const setState = (partial, replace) => {
+    // nextState => 변경 될 값, 함수로 넘어오면 함수 실행하고, 변수로 넘어오면 그대로 할당해준다.
     const nextState = typeof partial === "function" ? partial(state) : partial;
+    // 두개가 다른 값이면
     if (nextState !== state) {
+      // state를 previousState에 저장
       const previousState = state;
 
+      // replace가 있으면 state를 nextState로 바꾸고(완전 대체), 없으면 state, nextState의 두 개의 값이 병합됨
       state = replace ? nextState : Object.assign({}, state, nextState);
 
+      // 구독 해두었던 것들 실행
       listeners.forEach((listener) => listener(state, previousState));
     }
   };
@@ -27,13 +28,17 @@ export function create(createState) {
     return () => listeners.delete(listener);
   };
 
-  // 모든 리스너를 제거한다. 하지만 이미 정의된 상태를 초기화하진 않는다.
+  // 모든 리스너를 제거한다.
   const destroy = () => listeners.clear();
 
   const api = { setState, getState, subscribe, destroy };
 
+  console.log("createState", createState);
+
   // 인자로 전달받은 createState 함수를 이용하여 최초 상태를 설정한다.
   state = createState(setState, getState, api);
+
+  console.log("state", state);
 
   return api;
 }
